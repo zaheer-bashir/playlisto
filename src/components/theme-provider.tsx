f277eeb2a03fce25c41c66f8cf058e348909bf46
Ori Zehavi <orizehavi97@gmail.com>
2024-12-13 02:15:04 +0200

    Enhance theme provider and TypeScript configuration
    - Updated tsconfig.json to include path mapping for the next-themes package.
    - Refactored ThemeProvider component to define a more comprehensive ThemeProviderProps interface, improving type safety and clarity for props management.

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,7 +2,15 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+
+interface ThemeProviderProps {
+  children: React.ReactNode;
+  attribute?: string;
+  defaultTheme?: string;
+  enableSystem?: boolean;
+  storageKey?: string;
+  value?: { [x: string]: string };
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
